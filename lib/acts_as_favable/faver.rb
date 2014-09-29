1279d160ff8995f3df1989e937bf4f233139ff5e
tangjianhua <shmp2005@163.com>
2014-09-29 14:13:36 +0800

    make some methods private

--- lib/acts_as_favable/faver.rb
+++ lib/acts_as_favable/faver.rb
@@ -36,15 +36,6 @@ module ActsAsFavable
       favorites.where(extra_conditions)
     end
 
-    def find_favs_for_class klass, extra_conditions = {}
-      find_favs extra_conditions.merge({:favable_type => klass.name})
-    end
-
-    # Including polymporphic relations for eager loading
-    def include_objects
-      ActsAsFavable::Favorite.includes(:favable)
-    end
-
     def find_faved_items extra_conditions = {}
       options = extra_conditions.merge :faver_id => id, :faver_type => self.class.base_class.name
       include_objects.where(options).collect(&:favable)
@@ -52,6 +43,16 @@ module ActsAsFavable
 
     def get_faved klass, extra_conditions = {}
       klass.joins(:favs_for).merge find_favs(extra_conditions)
+    end
+
+    private
+    def find_favs_for_class klass, extra_conditions = {}
+      find_favs extra_conditions.merge({:favable_type => klass.name})
+    end
+
+    # Including polymporphic relations for eager loading
+    def include_objects
+      ActsAsFavable::Favorite.includes(:favable)
     end
   end
 end
